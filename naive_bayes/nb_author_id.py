cb64d7c62e364d742931bc5d9682cfd5c1ecd1c8
tfran <tfransson89@gmail.com>
2018-02-07 15:30:33 +0100

    Add first classifier and fit it

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,6 +14,7 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
 
 
 ### features_train and features_test are the features for the training
@@ -21,13 +22,12 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+# Create classifier object
+clf = GaussianNB()
+# Fit (train) the classifier
+clf.fit(features_train, labels_train)
+# Predict 
 
 
-
-#########################################################
-### your code goes here ###
-
-
-#########################################################
 
 
