76d6730c84b1297de39c2d7da2e4803e8ac1d72b
tfran <tfransson89@gmail.com>
2018-02-07 19:38:10 +0100

    Reduce training set

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,6 +19,9 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+# Reduce training sample
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
 
 from sklearn.svm import SVC
 
