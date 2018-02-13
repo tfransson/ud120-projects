114a0a9732c9ff45ba97a7b4b026b5e13f90ceae
tfran <tfransson89@gmail.com>
2018-02-13 14:55:13 +0100

    Outliers: Implement outlier rejection code (not on enron data)

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -10,11 +10,16 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
-    
-    cleaned_data = []
+    import numpy as np
+    #cleaned_data = []
 
     ### your code goes here
-
+    residual_errors = (predictions - net_worths)**2
+    tuple_list = zip(ages, net_worths, residual_errors)
+    dtype = [('age', int), ('net_worth', float), ('residual_error', float)]
+    structured_array = np.array(tuple_list, dtype = dtype)
+    structured_array = np.sort(structured_array, order='residual_error')
+    cleaned_data = structured_array[:int( 0.9*len(structured_array) )]
     
     return cleaned_data
 
--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -26,16 +26,14 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
 
+from sklearn.linear_model import LinearRegression
 
+reg = LinearRegression()
+reg.fit(ages_train, net_worths_train)
 
-
-
-
-
-
-
-
-
+print "\nThe slope is: %.2f" % reg.coef_
+print "The r^2 score on the test data is: %.3f" % reg.score(ages_test, net_worths_test)
+ 
 try:
     plt.plot(ages, reg.predict(ages), color="blue")
 except NameError:
@@ -64,10 +62,12 @@ if len(cleaned_data) > 0:
     ages, net_worths, errors = zip(*cleaned_data)
     ages       = numpy.reshape( numpy.array(ages), (len(ages), 1))
     net_worths = numpy.reshape( numpy.array(net_worths), (len(net_worths), 1))
-
+    
     ### refit your cleaned data!
     try:
         reg.fit(ages, net_worths)
+        print "\nThe slope after removing outliers is: %.2f" % reg.coef_
+        print "The r^2 score on the test data after removing outliers is: %.3f" % reg.score(ages_test, net_worths_test)
         plt.plot(ages, reg.predict(ages), color="blue")
     except NameError:
         print "you don't seem to have regression imported/created,"
