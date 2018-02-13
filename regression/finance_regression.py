7830f85a28ceb14322dbbdb46c3f673f3a4b537d
tfran <tfransson89@gmail.com>
2018-02-13 13:43:23 +0100

    regression: implement code to finish the lin. regression tasks

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -14,7 +14,7 @@
 
 
 import sys
-import pickle
+import cPickle as pickle
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
@@ -22,6 +22,7 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
+#features_list = ["bonus", "long_term_incentive"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -29,7 +30,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,11 +39,15 @@ test_color = "b"
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit(feature_train, target_train)
+print "\n"
+print "The slope is: %.2f" % reg.coef_
+print "The intercept is: %.2f" % reg.intercept_
 
-
-
-
-
+print "The r^2 score calculated on the training data is: %.3f" % reg.score(feature_train, target_train)
+print "The r^2 score calculated on the testing data is: %.3f" % reg.score(feature_test, target_test)
 
 
 ### draw the scatterplot, with color-coded training and testing points
@@ -64,6 +69,15 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b") 
+print "The slope is: %.2f" % reg.coef_
+print "The intercept is: %.2f" % reg.intercept_
+print "The r^2 score calculated on the training data is: %.3f" % reg.score(feature_train, target_train)
+print "The r^2 score calculated on the testing data is: %.3f" % reg.score(feature_test, target_test)
+
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
