a5d549f61c392716e670eb826b1170ad96ef9e60
tfran <tfransson89@gmail.com>
2018-02-13 16:07:31 +0100

    Outliers: Implement outlier removal on enron data

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import pickle
+import cPickle as pickle
 import sys
 import matplotlib.pyplot
 sys.path.append("../tools/")
@@ -9,11 +9,19 @@ from feature_format import featureFormat, targetFeatureSplit
 
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict.pop("TOTAL",0)
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
-
+from matplotlib import pyplot as plt
 ### your code below
+for point in data:
+	salary = point[0]
+	bonus = point[1]
+	plt.scatter(salary, bonus)
 
+plt.xlabel("salary")
+plt.ylabel("bonus")
+plt.show()
 
 
