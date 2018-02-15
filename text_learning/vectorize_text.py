58973a2e3287a0d161d839084a50b14d02db8683
tfran <tfransson89@gmail.com>
2018-02-15 14:32:51 +0100

    text learning: implement code for creating a dictionary of sara and chris' email using TfidfVectorizer

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
 import os
-import pickle
+import cPickle as pickle
 import re
 import sys
+
 
 sys.path.append( "../tools/" )
 from parse_out_email_text import parseOutText
@@ -11,7 +12,7 @@ from parse_out_email_text import parseOutText
 """
     Starter code to process the emails from Sara and Chris to extract
     the features and get the documents ready for classification.
-
+	
     The list of all the emails from Sara are in the from_sara list
     likewise for emails from Chris (from_chris)
 
@@ -28,36 +29,46 @@ from_chris = open("from_chris.txt", "r")
 
 from_data = []
 word_data = []
-
 ### temp_counter is a way to speed up the development--there are
 ### thousands of emails from Sara and Chris, so running over all of them
 ### can take a long time
 ### temp_counter helps you only look at the first 200 emails in the list so you
 ### can iterate your modifications quicker
 temp_counter = 0
-
+progress_counter = 0
 
 for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
+    	progress_counter += 1
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
+        # temp_counter += 1
         if temp_counter < 200:
-            path = os.path.join('..', path[:-1])
-            print path
+            path = os.path.join('..', path[:-2])
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            text_parsed = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
 
+            for word in ["sara", "shackleton", "chris", "germani"]:
+            	if word in text_parsed:
+            		text_parsed = text_parsed.replace(word, "")
+            	            	
             ### append the text to word_data
-
+            word_data.append(text_parsed)
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            if name == "sara":
+            	from_data.append(0)
+            else:
+            	from_data.append(1)
 
             email.close()
+
+            if progress_counter % 1000 == 0:
+    		print "%i emails processed..." % progress_counter
 
 print "emails processed"
 from_sara.close()
@@ -67,9 +78,18 @@ pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
-
-
-
 ### in Part 4, do TfIdf vectorization here
 
+# list of stopwords (use sklearns build-in stopword-list!!!)
+#from nltk.corpus import stopwords
+#sw = set(stopwords.words("english"))
 
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(stop_words = 'english')
+vectorizer.fit_transform(word_data)
+
+my_dictionary = vectorizer.get_feature_names()
+
+print "In total there are %i words in our dictionary." % len(my_dictionary)
+print "Word [34597] is %s" % my_dictionary[34597]
