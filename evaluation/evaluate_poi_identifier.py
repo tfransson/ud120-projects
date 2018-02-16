c07c9a1d4fb656e164924c59ba29a1fa936e09c0
tfran <tfransson89@gmail.com>
2018-02-16 17:45:52 +0100

    evaluate: Implement validation metrics and plot confusion matrix

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -11,7 +11,7 @@
     Start by loading/formatting the data...
 """
 
-import pickle
+import cPickle as pickle
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
@@ -28,4 +28,76 @@ labels, features = targetFeatureSplit(data)
 
 ### your code goes here 
 
+### Split data into training and testing sets
+from sklearn.model_selection import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size = 0.3, random_state = 42)
 
+### Decision tree classifier
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+	
+### Validate classifier
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score
+pred = clf.predict(features_test)
+acc = accuracy_score(labels_test, pred)
+print "The accuracy is %.3f" % acc
+
+### Made up prediction and true labels
+# Made up
+pred = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1]
+labels_test = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
+
+### Calulate precision, recall and f1 scores
+precision = precision_score(labels_test, pred)
+recall = recall_score(labels_test, pred)
+f1 = f1_score(labels_test, pred)
+print "Precision score: %.3f" % precision
+print "Recall score: %.3f" % recall
+print "F1 score: %.3f" % f1
+
+### Calculate confusion matrix
+conf = confusion_matrix(labels_test, pred)
+
+### Plot cofusion matrix
+from matplotlib import pyplot as plt
+import numpy as np
+import itertools
+class_names = ["not POI", "POI"]
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+plot_confusion_matrix(conf, class_names)
+plt.show()
