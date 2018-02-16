341eed0b0cecd9169200f48b47e89373be0e7659
tfran <tfransson89@gmail.com>
2018-02-16 15:11:03 +0100

    validation: start the final program by building the train/test splitting and accuracy.

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -10,7 +10,7 @@
     After that, it's not our code anymore--it's yours!
 """
 
-import pickle
+import cPickle as pickle
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
@@ -29,4 +29,17 @@ labels, features = targetFeatureSplit(data)
 
 ### it's all yours from here forward!  
 
+### Split data into training and testing sets
+from sklearn.model_selection import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size = 0.3, random_state = 42)
 
+### Decision tree classifier
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+
+### Estimate accuracy
+from sklearn.metrics import accuracy_score
+pred = clf.predict(features_test)
+acc = accuracy_score(labels_test, pred)
+print "The accuracy is %.3f" % acc
